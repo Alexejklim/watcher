2eba0460cfbe5a1a08de24183fb5eb21faccd8ba
Alexej <Alexej_klim@tut.by>
2019-07-24 08:05:30 +0300

    timechecker

---
+++ timechecker.py
@@ -0,0 +1,27 @@
+import datetime
+# https://stackoverflow.com/questions/2224742/most-recent-previous-business-day-in-python
+
+class Today():
+    def __init__(self):
+        pass
+
+    def get_current_datetime(self):
+        self.today = datetime.date.today()
+        self.weekno = today.weekday()
+        self.now = datetime.datetime.now()
+
+def time_in_range():
+    start = datetime.time(8, 0, 0)
+    end = datetime.time(10, 0, 0)
+    start = datetime.datetime.combine(today, start)
+    end = datetime.datetime.combine(today, end)
+    if end <= start:
+        end += datetime.timedelta(1) # tomorrow!
+    if currenttime <= start:
+        currenttime += datetime.timedelta(1) # tomorrow!
+    return start <= currenttime <= end
+
+if weekn o <5:
+    isBusinessday = True
+else:
+    isBusinessday = False
--- watcher_bot.py
+++ watcher_bot.py
@@ -1,37 +1,43 @@
 from io import BytesIO
 import logging
 import json
-
-raspberry = False
+import schedule
+from aiogram import Bot, Dispatcher, executor
+import timechecker
 
 config = {}
 with open('config.conf', 'r') as configFile:
     config = json.load(configFile)
 
+TOKEN = config['TOKEN']
+CHAT = config['CHAT']
+
+logging.basicConfig(level=logging.INFO)
+
+raspberry = False
+watcherisrun = True
+isBusinessday = False
+
 if raspberry:
     from raspi_bot import Raspberry
     RPi = Raspberry(config['Raspberry'])
-
-from aiogram import Bot, Dispatcher, executor
-logging.basicConfig(level=logging.INFO)
-
-TOKEN = config['TOKEN']
-CHAT = config['CHAT']
 
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
 
 async def broadcaster():
-    print(config['TOKEN'])
     while True:
+        schedule.run_pending()
+        isWorktime = time_in_range()
         stream = BytesIO()
         stream.seek(0)
         stream.truncate()
-        if raspberry and RPi.Muvement():
-            RPi.Capture(stream)
-            await bot.send_photo(CHAT,stream.getbuffer())
-            # time.sleep(0.2)
-            # await bot.send_message(CHAT, 'stream.getbuffer()')
+        if raspberry:
+            if watcherisrun and RPi.Muvement() and isBusinessday and isWorktime:
+                RPi.Capture(stream)
+                await bot.send_photo(CHAT,stream.getbuffer())
+                # time.sleep(0.2)
+                # await bot.send_message(CHAT, 'stream.getbuffer()')
 
 if __name__ == '__main__':
     executor.start(dp, broadcaster())
