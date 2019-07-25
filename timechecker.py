178b00a675399400178550154b6e2cb354b509c8
Alexejklim <Alexej_klim@tut.by>
2019-07-25 09:02:12 +0300

    timechecker refactored

--- timechecker.py
+++ timechecker.py
@@ -1,27 +1,15 @@
-from datetime import datetime
-# business days - pandas??
-# timezones! check in raspberry
-class Today(datetime):
-    def __init__(self):
-        pass
+from datetime import time, date, datetime
 
-    def get_current_datetime(self):
-        self.today = datetime.date.today()
-        self.weekno = today.weekday()
-        self.now = datetime.datetime.now()
+class Today:
+    def __init__(self,config):
+        self.timebegin = time(config['begin'], 0, 0)
+        self.timeend = time(config['end'], 0, 0)
 
-def time_in_range():
-    start = datetime.time(8, 0, 0)
-    end = datetime.time(10, 0, 0)
-    start = datetime.datetime.combine(today, start)
-    end = datetime.datetime.combine(today, end)
-    if end <= start:
-        end += datetime.timedelta(1) # tomorrow!
-    if currenttime <= start:
-        currenttime += datetime.timedelta(1) # tomorrow!
-    return start <= currenttime <= end
+    def isBusinessday(self):    
+        return date.today().weekday() < 5
 
-if weekn o <5:
-    isBusinessday = True
-else:
-    isBusinessday = False
+    def isWorktime(self):
+        self.begin = datetime.combine(date.today(), self.timebegin)
+        self.end = datetime.combine(date.today(), self.timeend)
+        return self.begin <= datetime.now() <= self.end
+  
--- watcher_bot.py
+++ watcher_bot.py
@@ -2,7 +2,7 @@ import logging
 import json
 import schedule
 from aiogram import Bot, Dispatcher, executor
-import timechecker
+from timechecker import Today
 
 config = {}
 with open('config.conf', 'r') as configFile:
@@ -15,20 +15,21 @@ logging.basicConfig(level=logging.INFO)
 
 raspberry = False
 watcherisrun = True
-isBusinessday = False
+
+bot = Bot(TOKEN)
+dp = Dispatcher(bot)
 
 if raspberry:
     from raspi_bot import Raspberry
     RPi = Raspberry(config['Raspberry'])
-
-bot = Bot(TOKEN)
-dp = Dispatcher(bot)
+    
+today = Today(config['Time'])
 
 async def broadcaster():
     while True:
         schedule.run_pending()
         if raspberry:
-            if watcherisrun and RPi.Muvement() and isBusinessday and isWorktime:
+            if watcherisrun and RPi.Muvement() and today.isBusinessday and today.isWorktime:
                 await bot.send_photo(CHAT,RPi.Capture())
                 # time.sleep(0.2)
                 # await bot.send_message(CHAT, 'stream.getbuffer()')
